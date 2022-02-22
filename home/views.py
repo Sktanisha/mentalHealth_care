91291757153e057b296930e7848f04e20ea27bd3
Sk. Sanjida Tanisha <sanjidatanisha99@gmail.com>
2022-02-23 00:24:04 +0600

    register models and backend

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
+from home.models import  accounts
 
 # Create your views here.
 def index(request):
@@ -9,5 +10,24 @@ def index(request):
 def login(request):
     return render(request, 'login.html')
 
+# def register(request):
+#     return render(request, 'register.html')
+
 def register(request):
-    return render(request, 'register.html')
+    context = {}
+    if request.method == 'POST':
+        if request.POST.get('name') and request.POST.get('age') and request.POST.get('address') and request.POST.get('password') and request.POST.get('email') and request.POST.get('mobile'):
+            saveRecord = accounts()
+
+            saveRecord.name = request.POST.get('name')
+            saveRecord.age = request.POST.get('age')
+            saveRecord.address = request.POST.get('address')
+            saveRecord.password = request.POST.get('password')
+            saveRecord.email = request.POST.get('email')
+            saveRecord.mobile = request.POST.get('mobile')
+
+            saveRecord.save()
+            return HttpResponse("added successfully !!")
+
+    else:
+        return render(request, 'register.html', context)
